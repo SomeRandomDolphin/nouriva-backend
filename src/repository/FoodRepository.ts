5ea88e3709edba9a856b4dc63e0997ae2ad8f527
Ranto Bastara Hamonangan Sitorus <rantositorus44@gmail.com>
2024-04-15 22:09:32 +0700

    fix: GetFoodTypebyID not found endpoint

--- src/repository/FoodRepository.ts
+++ src/repository/FoodRepository.ts
@@ -28,26 +28,10 @@ export const queryFoodDetailAll = async () => {
   return data;
 };
 
-export const queryFoodTypeDetailbyID = async (idInput: number) => {
-  const data = await db.foodType.findUnique({
-    where: {
-      id: idInput,
-    },
-    select: {
-      id: true,
-      name: true,
-    },
-  });
-  if (!data) throw new CustomError(404, "Food Type not found");
-  return data;
-};
-
 export const queryFoodWithFoodType = async (idInput: number) => {
-  const { id } = await queryFoodTypeDetailbyID(idInput);
-
   const data = await db.food.findMany({
     where: {
-      foodTypeId: id,
+      foodTypeId: idInput,
     },
     select: {
       id: true,
@@ -61,6 +45,6 @@ export const queryFoodWithFoodType = async (idInput: number) => {
       foodTypeId: true,
     },
   });
-  if (!data) throw new CustomError(404, "Food not found");
+  if (data.length === 0) throw new CustomError(404, "Food not found");
   return data;
 };
